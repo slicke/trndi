23cde8f8efb458146a074045c2c6e11c780cc4fd
Björn Lindh <bjorn.lindh@gmail.com>
2025-03-29 15:22:51 +0100

    Add Qt6 tweaks for the floaing window

--- ufloat.pp
+++ ufloat.pp
@@ -13,9 +13,15 @@ uses
   LCLIntf, LCLType, InterfaceBase
   {$IFDEF DARWIN},
     CocoaAll
+  {$ENDIF}
+  {$IFDEF LCLQT6},
+   qt6, qtwidgets, qtobjects, qtint
   {$ENDIF};
 
 type
+
+  { TfFloat }
+
   TfFloat = class(TForm)
     lArrow: TLabel;
     lVal: TLabel;
@@ -32,9 +38,12 @@ type
     procedure FormKeyPress(Sender: TObject; var Key: char);
     procedure FormMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState;
       X, Y: Integer);
+    procedure FormMouseEnter(Sender: TObject);
+    procedure FormMouseLeave(Sender: TObject);
     procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
     procedure FormMouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X,
       Y: Integer);
+    procedure FormPaint(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure lValMouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X,
       Y: Integer);
@@ -43,6 +52,7 @@ type
   private
     procedure SetFormOpacity(Opacity: Double);
     procedure ApplyRoundedCorners;
+    procedure CreateRoundedCorners;
   public
 
   end;
@@ -60,20 +70,44 @@ implementation
 {$R *.lfm}
 
 procedure TfFloat.FormCreate(Sender: TObject);
+{$IF DEFINED(LCLQt6)}
+var
+  QtWidget: TQtWidget;
+{$ENDIF}
 begin
   // We dont want any borders, as its a float
   BorderStyle := bsNone;
   {$IFNDEF DARWIN}
   Color := clWhite; //Set the background color
+   {$endif}
+   {$IFDEF LCLQt6}
+  if HandleAllocated then
+  begin
+    QtWidget := TQtWidget(Handle);
+    if Assigned(QtWidget) and Assigned(QtWidget.Widget) then
+    begin
+      // Set translucent background
+      QtWidget.setAttribute(QtWA_TranslucentBackground, True);
+
+      // Set frameless window hint
+      QtWidget.setWindowFlags(QtWidget.windowFlags or QtFramelessWindowHint);
+    end;
+  end;
   {$ENDIF}
 end;
 
 procedure TfFloat.ApplyRoundedCorners;
-{$IFDEF DARWIN}
 var
+{$IFDEF DARWIN}
   NSViewHandle: NSView;
   NSWin: NSWindow;
   Mask: NSBezierPath;
+{$ENDIF}
+{$IFDEF Windows}
+ ABitmap: TBitmap;
+{$ENDIF}
+{$IFDEF LCLQT6}
+  StyleStr: WideString;
 {$ENDIF}
 begin
   {$IFDEF DARWIN}
@@ -106,10 +140,19 @@ begin
   except
     // Ignore any errors
   end;
+  {$ENDIF}
+  {$IFDEF LCLQT6}
+    StyleStr := 'border-radius: 10px; background-color: rgba(240, 240, 240, 255);';
+  Self.BorderStyle := bsNone; // Remove border
+if HandleAllocated then
+QWidget_setStyleSheet(TQtWidget(Handle).Widget,
+  @stylestr);
+                                            CreateRoundedCorners;
   {$ELSE}
-  // Use LCL stuff when not Mac
-  var ABitmap := TBitmap.Create;
+  Self.BorderStyle := bsNone; // Remove border
+  // Use LCL stuff when Windows (or not Qt really)
   try
+    ABitmap := TBitmap.Create;
     ABitmap.Monochrome := True;
     ABitmap.Width := Width;
     ABitmap.Height := Height;
@@ -134,6 +177,10 @@ var
   NSViewHandle: NSView;
   NSWin: NSWindow;
 {$ENDIF}
+{$IFDEF LCLQt6}
+var
+  StyleStr: WideString;
+{$ENDIF}
 begin
   {$IFDEF DARWIN}
   if HandleAllocated then
@@ -153,9 +200,17 @@ begin
     end;
   end;
   {$ELSE}
-  // Standard LCL approach för andra plattformar
-  AlphaBlend := Opacity < 1.0;
-  AlphaBlendValue := Round(Opacity * 255);
+    {$IFDEF LCLQt6}
+    if HandleAllocated then
+    begin
+      // For Qt6, use style sheets to set opacity
+      StyleStr := Format('background-color: rgba(240, 240, 240, %.0f);', [Opacity * 255]);
+      QWidget_setStyleSheet(TQtWidget(Handle).Widget, @StyleStr);
+    end;
+    {$ENDIF}
+      // Standard LCL approach for other platforms
+      AlphaBlend := Opacity < 1.0;
+      AlphaBlendValue := Round(Opacity * 255);
   {$ENDIF}
 end;
 
@@ -192,19 +247,102 @@ end;
 
 procedure TfFloat.FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
 begin
+  {$ifndef LCLQt6}
   if DraggingWin then
   begin
     SetBounds(Left + (X - PX), Top + (Y - PY), Width, Height);
   end;
+  {$endif}
 end;
 
 procedure TfFloat.FormMouseUp(Sender: TObject; Button: TMouseButton; Shift:
   TShiftState; X, Y: Integer);
 begin
-  DraggingWin := false;
-  {$ifdef DARWIN}
-     BorderStyle:= bsNone;
+  {$ifndef LCLQt6}
+    DraggingWin := false;
+    {$ifdef DARWIN}
+       BorderStyle:= bsNone;
+    {$endif}
   {$endif}
+end;
+
+
+procedure TfFloat.CreateRoundedCorners;
+{$IFDEF LCLQt6}
+var
+  Path: QPainterPathH;
+  Painter: QPainterH;
+  QtWidget: TQtWidget;
+  XBrush: QBrushH;
+  Pen: QPenH;
+  WidgetHandle: QWidgetH;
+  BlackColor: QColorH;
+  PaintDevice: QPaintDeviceH;
+  bgcol: QtGlobalColor;
+begin
+
+  bgcol := QtGlobalColor.Qtgreen;
+  // Konvertera LCL handle till Qt widget
+  QtWidget := TQtWidget(Handle);
+  WidgetHandle := QtWidget.GetContainerWidget;
+
+  // Se till att widgeten har transparent bakgrund
+  QWidget_setAttribute(WidgetHandle, QtWA_TranslucentBackground, true);
+
+
+  // Skapa QColor för svart
+  BlackColor := QColor_create(0, 0, 0);
+
+  // Hämta paint device från widget
+  PaintDevice := QWidget_to_QPaintDevice(WidgetHandle);
+
+  // Skapa målare direkt på widgeten
+  Painter := QPainter_create();
+  QPainter_begin(Painter, PaintDevice);
+
+  // Aktivera antialiasing
+  QPainter_setRenderHint(Painter, QPainterAntialiasing, true);
+
+  // Skapa en path för rundade hörn
+  Path := QPainterPath_create();
+  QPainterPath_addRoundedRect(Path, 0, 0, Width, Height, 20, 20);
+
+  // Skapa pensel för fyllning
+  XBrush := QBrush_create();
+  QBrush_setStyle(XBrush, QtSolidPattern);
+  // För QBrush_setColor används QtGlobalColor enum
+  QBrush_setColor(XBrush, bgcol);
+
+  // Skapa penna för kontur
+  Pen := QPen_create();
+  QPen_setStyle(Pen, QtSolidLine);
+  // För QPen_setColor används PQColor (QColorH)
+  QPen_setColor(Pen, @BlackColor);
+  QPen_setWidth(Pen, 1);
+
+  // Rita direkt på widgeten
+  QPainter_setPen(Painter, Pen);
+  QPainter_setBrush(Painter, XBrush);
+  QPainter_drawPath(Painter, Path);
+
+  // Avsluta målning
+  QPainter_end(Painter);
+
+  // Städa upp
+  QPainterPath_destroy(Path);
+  QBrush_destroy(XBrush);
+  QPen_destroy(Pen);
+  QColor_destroy(BlackColor);
+  QPainter_destroy(Painter);
+  {$ENDIF}
+end;
+
+
+
+procedure TfFloat.FormPaint(Sender: TObject);
+begin
+  CreateRoundedCorners;
+
 end;
 
 procedure TfFloat.FormMouseDown(Sender: TObject; Button: TMouseButton; Shift:
@@ -215,6 +353,20 @@ begin
   PY := Y;
 end;
 
+procedure TfFloat.FormMouseEnter(Sender: TObject);
+begin
+ {$ifdef LCLQt6}
+   BorderStyle := bsToolWindow;
+ {$endif}
+end;
+
+procedure TfFloat.FormMouseLeave(Sender: TObject);
+begin
+   {$ifdef LCLQt6}
+   BorderStyle := bsNone;
+ {$endif}
+end;
+
 procedure TfFloat.FormKeyPress(Sender: TObject; var Key: char);
 begin
 end;
@@ -223,6 +375,10 @@ procedure TfFloat.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
 var
   num: double;
 begin
+  if key = VK_ESCAPE then begin
+    Hide;
+    key := 0;
+  end;
   if not ((ssShift in Shift) and (Key >= 48) and (Key <= 57)) then
     Exit;
   num := (key-48) / 10;
