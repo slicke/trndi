76016a2dc7e32c0d0a3aba2255683887645d3193
Björn Lindh <bjorn.lindh@qte.se>
2025-04-07 17:58:34 +0200

    Fix last value position when expanded

--- trndi.native.pp
+++ trndi.native.pp
@@ -1,6 +1,6 @@
 (*
- * This file is part of Trndi (https://github.com/xxxx or http://xxx.github.io).
- * Copyright (c) 2021-24 Björn Lindh.
+ * This file is part of Trndi (https://github.com/slicke/trndi or http://xxx.github.io).
+ * Copyright (c) 2021-25 Björn Lindh.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
