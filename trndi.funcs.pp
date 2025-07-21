70da4298eaf74a0b544c5dd2144f5cfb3e466a1e
Björn Lindh <bjorn.lindh@gmail.com>
2025-07-21 15:35:56 +0200

    Add version check

--- trndi.funcs.pp
+++ trndi.funcs.pp
@@ -8,7 +8,9 @@ unit trndi.funcs;
 interface
 
 uses
-  Classes, SysUtils, ExtCtrls, stdctrls, graphics, trndi.types, forms, math{$ifdef DARWIN}, CocoaAll{$endif};
+  Classes, SysUtils, ExtCtrls, stdctrls, graphics, trndi.types, forms, math,
+  fpjson, jsonparser, dateutils
+  {$ifdef DARWIN}, CocoaAll{$endif};
 
 
 procedure CenterPanelToCaption(Panel: TPanel);
@@ -18,6 +20,11 @@ procedure PaintLbl(Sender: TLabel; OutlineWidth: integer = 1; OutlineColor: TCol
 procedure LogMessage(const Msg: string);
 procedure SortReadingsDescending(var Readings: array of BGReading);
 procedure SetPointHeight(L: TLabel; Value: Single; clientHeight: integer);
+function GetNewerVersionName(const JsonResponse: string; IncludePrerelease: Boolean = False): string;
+function ParseCompilerDate: TDateTime;
+function GetNewerVersionURL(const JsonResponse: string;
+                            IncludePrerelease: Boolean = False;
+                            Platform: string = ''): string;
 
 const
 INTERVAL_MINUTES = 5; // Each time interval is 5 minutes
@@ -30,6 +37,9 @@ BG_REFRESH = 300000; // 5 min refresh
 
 DOT_GRAPH =  '•';
 DOT_FRESH = '☉';
+
+APP_BUILD_DATE = '2024/06/01'; // {$I %DATE%}; // Returns "2025/07/21"
+APP_BUILD_TIME = {$I %TIME%}; // Returns "14:30:25"
 
 var
 MAX_MIN: integer = 1440; // Max time to request
@@ -223,6 +233,246 @@ procedure setTimeRange(mins, count: integer);
 begin
   MAX_MIN := min(mins, 20); // Max time to request
   MAX_RESULT := min(count, 2); // Max results
+end;
+
+function ParseCompilerDate: TDateTime;
+var
+  DateParts: TStringArray;
+  TimeParts: TStringArray;
+  Year, Month, Day, Hour, Min, Sec: Word;
+begin
+  // Parse date: "2025/07/21"
+  DateParts := APP_BUILD_DATE.Split('/');
+  Year := StrToInt(DateParts[0]);
+  Month := StrToInt(DateParts[1]);
+  Day := StrToInt(DateParts[2]);
+
+  // Parse time: "14:30:25"
+  TimeParts := APP_BUILD_TIME.Split(':');
+  Hour := StrToInt(TimeParts[0]);
+  Min := StrToInt(TimeParts[1]);
+  Sec := StrToInt(TimeParts[2]);
+
+  Result := EncodeDateTime(Year, Month, Day, Hour, Min, Sec, 0);
+end;
+
+function GetNewerVersionName(const JsonResponse: string;
+                             IncludePrerelease: Boolean = False): string;
+var
+  JsonData: TJSONData;
+  JsonObj: TJSONObject;
+  JsonArray: TJSONArray;
+  ReleaseDateStr: string;
+  ReleaseDate, BuildDate: TDateTime;
+  Year, Month, Day, Hour, Min, Sec: Word;
+  i: Integer;
+  IsPrerelease: Boolean;
+begin
+  Result := ''; // Empty if no newer version
+  JsonData := nil;
+
+  try
+    // Use parsed compile-time date/time
+    BuildDate := ParseCompilerDate;
+
+    // Parse JSON
+    JsonData := GetJSON(JsonResponse);
+
+    // Handle both single release object and array of releases
+    if JsonData is TJSONArray then
+    begin
+      // Array of releases (from /releases endpoint)
+      JsonArray := TJSONArray(JsonData);
+
+      for i := 0 to JsonArray.Count - 1 do
+      begin
+        JsonObj := TJSONObject(JsonArray[i]);
+        IsPrerelease := JsonObj.Get('prerelease', False);
+
+        // Skip prereleases if not wanted
+        if not IncludePrerelease and IsPrerelease then
+          Continue;
+
+        ReleaseDateStr := JsonObj.Get('published_at', '');
+        if Length(ReleaseDateStr) >= 19 then
+        begin
+          // Parse date and compare
+          Year := StrToInt(Copy(ReleaseDateStr, 1, 4));
+          Month := StrToInt(Copy(ReleaseDateStr, 6, 2));
+          Day := StrToInt(Copy(ReleaseDateStr, 9, 2));
+          Hour := StrToInt(Copy(ReleaseDateStr, 12, 2));
+          Min := StrToInt(Copy(ReleaseDateStr, 15, 2));
+          Sec := StrToInt(Copy(ReleaseDateStr, 18, 2));
+
+          ReleaseDate := EncodeDateTime(Year, Month, Day, Hour, Min, Sec, 0);
+
+          if ReleaseDate > BuildDate then
+          begin
+            Result := JsonObj.Get('name', JsonObj.Get('tag_name', ''));
+            Exit; // Found newer version, return it
+          end;
+        end;
+      end;
+    end
+    else if JsonData is TJSONObject then
+    begin
+      // Single release object (from /releases/latest endpoint)
+      JsonObj := TJSONObject(JsonData);
+      ReleaseDateStr := JsonObj.Get('published_at', '');
+
+      if Length(ReleaseDateStr) >= 19 then
+      begin
+        // Parse ISO date
+        Year := StrToInt(Copy(ReleaseDateStr, 1, 4));
+        Month := StrToInt(Copy(ReleaseDateStr, 6, 2));
+        Day := StrToInt(Copy(ReleaseDateStr, 9, 2));
+        Hour := StrToInt(Copy(ReleaseDateStr, 12, 2));
+        Min := StrToInt(Copy(ReleaseDateStr, 15, 2));
+        Sec := StrToInt(Copy(ReleaseDateStr, 18, 2));
+
+        ReleaseDate := EncodeDateTime(Year, Month, Day, Hour, Min, Sec, 0);
+
+        if ReleaseDate > BuildDate then
+          Result := JsonObj.Get('name', JsonObj.Get('tag_name', ''));
+      end;
+    end;
+
+  except
+    Result := ''; // On any error, return empty
+  end;
+
+  if Assigned(JsonData) then
+    JsonData.Free;
+end;
+function GetNewerVersionURL(const JsonResponse: string;
+                                  IncludePrerelease: Boolean = False;
+                                  Platform: string = ''): string;
+var
+  JsonData: TJSONData;
+  JsonObj: TJSONObject;
+  JsonArray: TJSONArray;
+  ReleaseDateStr, ReleaseTitle: string;
+  ReleaseDate, BuildDate: TDateTime;
+  Year, Month, Day, Hour, Min, Sec: Word;
+  i: Integer;
+  IsPrerelease, MatchesPlatform: Boolean;
+begin
+  Result := ''; // Empty if no newer version
+  JsonData := nil;
+
+  try
+    BuildDate := ParseCompilerDate;
+    JsonData := GetJSON(JsonResponse);
+
+    // Handle single release object (from /releases/latest)
+    if JsonData is TJSONObject then
+    begin
+      JsonObj := TJSONObject(JsonData);
+
+      // Check platform filter
+      if Platform <> '' then
+      begin
+        ReleaseTitle := JsonObj.Get('name', JsonObj.Get('tag_name', ''));
+        MatchesPlatform := False;
+
+        case LowerCase(Platform) of
+          'windows': MatchesPlatform := Pos('windows', LowerCase(ReleaseTitle)) > 0;
+          'mac', 'macos': MatchesPlatform := (Pos('mac', LowerCase(ReleaseTitle)) > 0) or
+                                           (Pos('macos', LowerCase(ReleaseTitle)) > 0);
+          'linux': MatchesPlatform := Pos('linux', LowerCase(ReleaseTitle)) > 0;
+        else
+          MatchesPlatform := Pos(LowerCase(Platform), LowerCase(ReleaseTitle)) > 0;
+        end;
+
+        if not MatchesPlatform then
+          Exit; // No match, return empty
+      end;
+
+      // Check date
+      ReleaseDateStr := JsonObj.Get('published_at', '');
+      if Length(ReleaseDateStr) >= 19 then
+      begin
+        Year := StrToInt(Copy(ReleaseDateStr, 1, 4));
+        Month := StrToInt(Copy(ReleaseDateStr, 6, 2));
+        Day := StrToInt(Copy(ReleaseDateStr, 9, 2));
+        Hour := StrToInt(Copy(ReleaseDateStr, 12, 2));
+        Min := StrToInt(Copy(ReleaseDateStr, 15, 2));
+        Sec := StrToInt(Copy(ReleaseDateStr, 18, 2));
+
+        ReleaseDate := EncodeDateTime(Year, Month, Day, Hour, Min, Sec, 0);
+
+        if ReleaseDate > BuildDate then
+          Result := JsonObj.Get('html_url', ''); // Return URL for single release
+      end;
+    end
+
+    // Handle array of releases (from /releases)
+    else if JsonData is TJSONArray then
+    begin
+      JsonArray := TJSONArray(JsonData);
+
+      for i := 0 to JsonArray.Count - 1 do
+      begin
+        JsonObj := TJSONObject(JsonArray[i]);
+        IsPrerelease := JsonObj.Get('prerelease', False);
+
+        // Filter by prerelease setting
+        if IncludePrerelease then
+        begin
+          if not IsPrerelease then Continue; // Want prerelease, skip stable
+        end
+        else
+        begin
+          if IsPrerelease then Continue; // Want stable, skip prerelease
+        end;
+
+        // Check platform filter
+        if Platform <> '' then
+        begin
+          ReleaseTitle := JsonObj.Get('name', JsonObj.Get('tag_name', ''));
+          MatchesPlatform := False;
+
+          case LowerCase(Platform) of
+            'windows': MatchesPlatform := Pos('windows', LowerCase(ReleaseTitle)) > 0;
+            'mac', 'macos': MatchesPlatform := (Pos('mac', LowerCase(ReleaseTitle)) > 0) or
+                                             (Pos('macos', LowerCase(ReleaseTitle)) > 0);
+            'linux': MatchesPlatform := Pos('linux', LowerCase(ReleaseTitle)) > 0;
+          else
+            MatchesPlatform := Pos(LowerCase(Platform), LowerCase(ReleaseTitle)) > 0;
+          end;
+
+          if not MatchesPlatform then
+            Continue;
+        end;
+
+        // Check date
+        ReleaseDateStr := JsonObj.Get('published_at', '');
+        if Length(ReleaseDateStr) >= 19 then
+        begin
+          Year := StrToInt(Copy(ReleaseDateStr, 1, 4));
+          Month := StrToInt(Copy(ReleaseDateStr, 6, 2));
+          Day := StrToInt(Copy(ReleaseDateStr, 9, 2));
+          Hour := StrToInt(Copy(ReleaseDateStr, 12, 2));
+          Min := StrToInt(Copy(ReleaseDateStr, 15, 2));
+          Sec := StrToInt(Copy(ReleaseDateStr, 18, 2));
+
+          ReleaseDate := EncodeDateTime(Year, Month, Day, Hour, Min, Sec, 0);
+
+          if ReleaseDate > BuildDate then
+          begin
+            Result := JsonObj.Get('html_url', ''); // Return URL
+            Exit; // Found it, exit
+          end;
+        end;
+      end;
+    end;
+
+  except
+    Result := '';
+  end;
+
+  if Assigned(JsonData) then
+    JsonData.Free;
 end;
 
 end.
