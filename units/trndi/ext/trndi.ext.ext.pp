940612e62a380ca42f8fdd5078333bd81794cc60
Björn Lindh <bjorn.lindh@gmail.com>
2025-11-25 14:17:32 +0100

    Update copyright

--- units/trndi/ext/trndi.ext.ext.pp
+++ units/trndi/ext/trndi.ext.ext.pp
@@ -1,22 +1,40 @@
 
 (*
- * This file is part of Trndi (https://github.com/slicke/trndi).
- * Copyright (c) 2021-2025 Björn Lindh.
+ * Trndi
+ * Medical and Non-Medical Usage Alert
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * ---------
- *
+ * Copyright (c) Björn Lindh
  * GitHub: https://github.com/slicke/trndi
+ *
+ * This program is distributed under the terms of the GNU General Public License,
+ * Version 3, as published by the Free Software Foundation. You may redistribute
+ * and/or modify the software under the terms of this license.
+ *
+ * A copy of the GNU General Public License should have been provided with this
+ * program. If not, see <http://www.gnu.org/licenses/gpl.html>.
+ *
+ * ================================== IMPORTANT ==================================
+ * MEDICAL DISCLAIMER:
+ * - This software is NOT a medical device and must NOT replace official continuous
+ *   glucose monitoring (CGM) systems or any healthcare decision-making process.
+ * - The data provided may be delayed, inaccurate, or unavailable.
+ * - DO NOT make medical decisions based on this software.
+ * - VERIFY all data using official devices and consult a healthcare professional for
+ *   medical concerns or emergencies.
+ *
+ * LIABILITY LIMITATION:
+ * - The software is provided "AS IS" and without any warranty—expressed or implied.
+ * - Users assume all risks associated with its use. The developers disclaim all
+ *   liability for any damage, injury, or harm, direct or incidental, arising
+ *   from its use.
+ *
+ * INSTRUCTIONS TO DEVELOPERS & USERS:
+ * - Any modifications to this file must include a prominent notice outlining what was
+ *   changed and the date of modification (as per GNU GPL Section 5).
+ * - Distribution of a modified version must include this header and comply with the
+ *   license terms.
+ *
+ * BY USING THIS SOFTWARE, YOU AGREE TO THE TERMS AND DISCLAIMERS STATED HERE.
  *)
 
 unit trndi.ext.ext;
