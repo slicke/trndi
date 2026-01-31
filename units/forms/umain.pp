1555f1ec5d03b519a90d6b5e92287c9de7aac786
Björn Lindh <slicke@users.noreply.github.com>
2026-01-31 14:39:10 +0100

    Auto load basal

--- units/forms/umain.pp
+++ units/forms/umain.pp
@@ -141,7 +141,6 @@ TfBG = class(TForm)
   lPredict: TLabel;
   miGuidelines: TMenuItem;
   miBasalRate: TMenuItem;
-  miShowBasalOverlay: TMenuItem;
   miReadingsSince: TMenuItem;
   miExtLog: TMenuItem;
   miSep1: TMenuItem;
@@ -263,7 +262,7 @@ TfBG = class(TForm)
   procedure lDiffClick(Sender: TObject);
   procedure lPredictClick(Sender: TObject);
   procedure miBasalRateClick(Sender: TObject);
-  procedure miShowBasalOverlayClick(Sender: TObject);
+  procedure AutoEnableBasalOverlay;
   procedure miDNSClick(Sender: TObject);
   procedure miDotNormalDrawItem(Sender: TObject; ACanvas: TCanvas;
     ARect: TRect; AState: TOwnerDrawState);
@@ -1005,58 +1004,22 @@ begin
     ShowMessage(res.toString);
 end;
 
-procedure TfBG.miShowBasalOverlayClick(Sender: TObject);
+procedure TfBG.AutoEnableBasalOverlay;
 var
   profile: TBasalProfile;
   ok: boolean;
-  i: integer;
-  s: string;
-  h, m: integer;
 begin
-  miShowBasalOverlay.Checked := not miShowBasalOverlay.Checked;
-  if miShowBasalOverlay.Checked then
+  if not api.supportsBasal then
+    Exit;
+
+  ok := api.getBasalProfile(profile);
+  if not ok then
+    Exit;
+
+  if fHistoryGraph <> nil then
   begin
-    if not api.supportsBasal then
-    begin
-      ShowMessage('Backend does not support basal profiles');
-      miShowBasalOverlay.Checked := False;
-      Exit;
-    end;
-
-    ok := api.getBasalProfile(profile);
-    if not ok then
-    begin
-      ShowMessage('Failed to fetch basal profile: ' + api.errormsg);
-      miShowBasalOverlay.Checked := False;
-      Exit;
-    end;
-
-    { Debug: show textual list of profile entries like "00:00 1.2" }
-    s := '';
-    for i := 0 to Length(profile) - 1 do
-    begin
-      h := profile[i].startMin div 60;
-      m := profile[i].startMin mod 60;
-      if s <> '' then
-        s := s + sLineBreak;
-      s := s + Format('%.2d:%.2d %.1f', [h, m, profile[i].value]);
-    end;
-    ShowMessage(s);
-
-    if fHistoryGraph = nil then
-      ShowHistoryGraph(bgs, un, CurrentHistoryGraphPalette,
-        api.cgmHi, api.cgmLo, api.cgmRangeHi, api.cgmRangeLo);
-
-    if fHistoryGraph <> nil then
-    begin
-      fHistoryGraph.SetBasalProfile(profile);
-      fHistoryGraph.SetBasalOverlayEnabled(True);
-    end;
-  end
-  else
-  begin
-    if fHistoryGraph <> nil then
-      fHistoryGraph.SetBasalOverlayEnabled(False);
+    fHistoryGraph.SetBasalProfile(profile);
+    fHistoryGraph.SetBasalOverlayEnabled(True);
   end;
 end;
 
@@ -1318,6 +1281,7 @@ begin
     
     ShowHistoryGraph(readings, un, CurrentHistoryGraphPalette,
       api.cgmHi, api.cgmLo, api.cgmRangeHi, api.cgmRangeLo);
+    AutoEnableBasalOverlay;
   end;
 end;
 
@@ -2587,12 +2551,14 @@ begin
 
   ShowHistoryGraph(readings, un, CurrentHistoryGraphPalette, 
     api.cgmHi, api.cgmLo, api.cgmRangeHi, api.cgmRangeLo);
+  AutoEnableBasalOverlay;
 end;
 
 procedure TfBG.miHistoryClick(Sender: TObject);
 begin
   ShowHistoryGraph(bgs, un, CurrentHistoryGraphPalette,
     api.cgmHi, api.cgmLo, api.cgmRangeHi, api.cgmRangeLo);
+  AutoEnableBasalOverlay;
 end;
 
 procedure TfBG.miRangeColorClick(Sender: TObject);
