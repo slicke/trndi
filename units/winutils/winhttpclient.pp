9176e524718606e5cbcf235f440f35cd4cb91618
Björn Lindh <slicke@users.noreply.github.com>
2026-02-02 19:18:26 +0100

    Add more debug

--- units/winutils/winhttpclient.pp
+++ units/winutils/winhttpclient.pp
@@ -597,20 +597,25 @@ begin
 
         if not WinHttpReceiveResponse(hRequest, nil) then
         begin
+          dwSize := GetLastError;
+          LogMessageToFile('WinHTTP POST: ReceiveResponse failed with error: ' + IntToStr(dwSize));
           // If cert error with proxy, set ignore flags and retry
-          if (GetLastError = 12152) and Port.secure and (FProxyHost <> '') then
+          if (dwSize = 12152) and Port.secure and (FProxyHost <> '') then
           begin
             LogMessageToFile('WinHTTP POST: Cert error with proxy, setting flags and retrying');
             Flags := SECURITY_FLAG_IGNORE_UNKNOWN_CA or SECURITY_FLAG_IGNORE_CERT_DATE_INVALID or
                      SECURITY_FLAG_IGNORE_CERT_CN_INVALID or SECURITY_FLAG_IGNORE_CERT_WRONG_USAGE;
-            WinHttpSetOption(hRequest, WINHTTP_OPTION_SECURITY_FLAGS, @Flags, SizeOf(Flags));
+            if not WinHttpSetOption(hRequest, WINHTTP_OPTION_SECURITY_FLAGS, @Flags, SizeOf(Flags)) then
+              LogMessageToFile('WinHTTP POST: Failed to set flags on retry: ' + IntToStr(GetLastError));
             if not WinHttpReceiveResponse(hRequest, nil) then
-              raise Exception.Create('WinHttpReceiveResponse failed (' + IntToStr(GetLastError) + '): ' +
-                SysErrorMessage(GetLastError));
+              raise Exception.Create('WinHttpReceiveResponse retry failed (' + IntToStr(GetLastError) + '): ' +
+                SysErrorMessage(GetLastError))
+            else
+              LogMessageToFile('WinHTTP POST: Retry succeeded!');
           end
           else
-            raise Exception.Create('WinHttpReceiveResponse failed (' + IntToStr(GetLastError) + '): ' +
-              SysErrorMessage(GetLastError));
+            raise Exception.Create('WinHttpReceiveResponse failed (' + IntToStr(dwSize) + '): ' +
+              SysErrorMessage(dwSize));
         end;
 
         // Read response
